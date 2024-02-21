877d425fc0c788336105d59a2664553143069b8d
exodus696 <kickcscscscs@gmail.com>
2024-02-21 21:06:28 +0500

    fix incorrect order of exercises

--- db/models.py
+++ db/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from config import config
 
-engine = create_async_engine(url=config.db_url, echo=True)
+engine = create_async_engine(url=config.db_url, echo=False)
 sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
 
 
